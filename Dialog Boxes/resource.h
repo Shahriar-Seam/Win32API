8f9f8cf8c5a6c25fb9af8157f9f4db96049bebab
Shahriar-Seam <i.a.shahriarseam@gmail.com>
2024-09-14 17:03:47 +0600

    Dialog 1

---
+++ Dialog Boxes/resource.h
@@ -0,0 +1,10 @@
+#include <windows.h>
+#include <tchar.h>
+
+#define IDR_MYMENU 101
+#define IDI_MYICON 102
+#define IDD_ABOUT 103
+#define IDC_STATIC 104
+
+#define ID_FILE_EXIT 9001
+#define ID_HELP_ABOUT 9002
