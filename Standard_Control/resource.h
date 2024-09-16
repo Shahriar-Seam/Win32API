cc41b1e42a8dac1e1be427f381969a9771668a3f
Shahriar-Seam <i.a.shahriarseam@gmail.com>
2024-09-16 23:38:04 +0600

    Standard Control (Haven't even started properly yet)

---
+++ Standard_Control/resource.h
@@ -0,0 +1,11 @@
+#include <tchar.h>
+#include <windows.h>
+
+#define IDD_MAIN        101
+#define IDC_TEXT        1000
+#define IDC_NUMBER      1001
+#define IDC_LIST        1002
+#define IDC_ADD         1003
+#define IDC_CLEAR       1004
+#define IDC_REMOVE      1005
+#define IDC_SHOWCOUNT   1006
